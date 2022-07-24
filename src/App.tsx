b7fbf00d778ad3571143d62915a5051e5c4b7294
Alexander <nlr@users.noreply.github.com>
2022-07-24 22:42:56 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const params = [
+  { id: 1, name: 'Назначение' },
+  { id: 2, name: 'Длина' },
+  { id: 3, name: 'Высота' },
+  { id: 4, name: 'Что-то еще' },
+  { id: 5, name: 'Глубина' },
+  { id: 6, name: 'Белизна' },
+];
+
+const model = {
+  paramValues: [
+    { paramId: 1, value: 'повседневное' },
+    { paramId: 2, value: 'макси' },
+    { paramId: 3, value: 'высоко' },
+  ],
+};
+
+interface Param {
+  id: number;
+  name: string;
+  type?: `string`;
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+interface Model {
+  paramValues: ParamValue[];
+}
+
+type ParamEditorProps = {
+  params: Param[];
+  model: Model;
+};
+
+const ParamEditor = (props: ParamEditorProps) => {
+  const [model, setModel] = React.useState(props.model);
+
+  const getModel = () => {
+    return console.log(model);
+  };
+
+  const updateValue = (newValue: string, id: number) => {
+    let paramIndex = model.paramValues.findIndex(
+      (param) => param.paramId === id,
+    );
+
+    let paramValues = [...model.paramValues];
+
+    if (paramIndex === -1) {
+      paramValues = [...paramValues, { paramId: id, value: newValue }];
+      paramIndex = paramValues.length - 1; // new param index
+    }
+
+    const currentParam = { ...paramValues[paramIndex] };
+    const newParam = { ...currentParam, value: newValue };
+
+    paramValues[paramIndex] = newParam;
+
+    setModel((current) => {
+      return {
+        ...current,
+        paramValues: paramValues,
+      };
+    });
+  };
+
+  return (
+    <>
+      {props.params.map((value, index) => {
+        const inputDataIndex = model.paramValues.findIndex(
+          (param) => param.paramId === value.id,
+        );
+        const inputData =
+          inputDataIndex !== -1 ? model.paramValues[inputDataIndex].value : '';
+        return (
+          <div className="input" key={index}>
+            <span className="input--name">{value.name}</span>
+            <input
+              className="input--value"
+              defaultValue={inputData}
+              onChange={(e) => updateValue(e.currentTarget.value, value.id)}
+            />
+          </div>
+        );
+      })}
+      <button onClick={() => getModel()}>Получить в консоли модель</button>
+    </>
+  );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ParamEditor model={model} params={params} />;
 }
 
 export default App;
